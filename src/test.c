07822a7f5f8dbf7f8b975b41d7c94829400fe410
mojo <281188392@qq.com>
2026-09-23 17:18:17 +0800

    change a topic

--- src/test.c
+++ src/test.c
@@ -77,7 +77,7 @@ static uint16_t generate_mqtt_id(void)
 static struct mqtt_topic topics[] = {
 	{
 		.topic = {
-			.utf8 = (const uint8_t *)"topic1",
+			.utf8 = (const uint8_t *)"esp32_s2/percent",
 			.size = sizeof("topic1") - 1,
 		},
 		.qos = MQTT_QOS_0_AT_MOST_ONCE,
